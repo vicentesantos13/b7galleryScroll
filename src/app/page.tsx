e0e37430e20b4bc3a79dcf802aeefc9801eba3a5
Vicente Santos <vicentemoises.santos@gmail.com>
2023-12-13 16:58:53 -0300

    bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import  Gallery  from "@/components/Gallery";
+import Gallery from "@/components/Gallery";
 import { Head } from "@/components/Head";
 
 export default function Home() {
-  
   return (
     <main className="flex min-h-screen  mx-auto flex-col  items-center">
-      <Head/>
-      <Gallery/>
+      <Head />
+      <Gallery />
     </main>
   );
 }
--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -13,6 +13,7 @@ const Gallery = () => {
   });
 
   useEffect(() => {
+    
     getGallery();
   }, []);
 
